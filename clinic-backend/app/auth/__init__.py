d8d9bff6eca0394ba7b11136221e894e242eefd3
amaancodes <amaanmulla07@gmail.com>
2026-02-16 14:05:46 +0530

    updated init

--- clinic-backend/app/auth/__init__.py
+++ clinic-backend/app/auth/__init__.py
@@ -1,8 +1,4 @@
 """
 Auth domain module.
-
-Contains authentication-related API views, services, schemas, and (optionally)
-model facades. This module represents the \"auth\" bounded context in the
-modular monolith.
 """
 
